1109da93383978cab1879e899c8255a5f7e002c2
sanix <sanicksikani@gmail.com>
2020-09-08 18:23:09 +0300

    initial commit

---
+++ src/main/java/com/sanix/UserRegistrationSystem/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package com.sanix.UserRegistrationSystem.dto;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Users")
+public class UserDTO {
+
+    @Id
+    @GeneratedValue
+    @Column(name="USER_ID")
+    private Long id;
+
+    @Column(name="NAME")
+    private String name;
+
+    @Column(name="ADDRESS")
+    private String address;
+
+    @Column(name="EMAIL")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/sanix/UserRegistrationSystem/repository/UserJpaRepository.java
@@ -0,0 +1,9 @@
+package com.sanix.UserRegistrationSystem.repository;
+
+import com.sanix.UserRegistrationSystem.dto.UserDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends JpaRepository<UserDTO, Long> {
+
+    UserDTO findByName(String name);
+}
